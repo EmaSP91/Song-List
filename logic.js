c0c96aab30700db805184336bfd6d639f95aa2e2
Ema Aliaga <emakpop91@gmail.com>
2023-07-29 21:02:57 -0300

    Implement the Youtube API to add autoplay and a specific starting time of the song

--- logic.js
+++ logic.js
@@ -38,6 +38,8 @@ songForm.addEventListener('submit', function(event){
 })
 cancionAzar.addEventListener('click', mostrarCancion);
 
+cancionAzar.addEventListener('click', mostrarCancion);
+
 function mostrarCancion(){
     if (songList.length != 0){
   emptyList.style.display = "none";
@@ -46,8 +48,41 @@ function mostrarCancion(){
   const selectedSong = songList[randomIndex];
   console.log(songList)
   console.log(selectedSong)
+  //------
+ 
+  const apiKey = 'AIzaSyAVaBHWD19qKYWHRrPm1L4wkOHT6bwZ2UQ';
   
-  window.open(`https://www.youtube.com/results?search_query=${encodeURIComponent(selectedSong)}`);
+  const url = `https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(selectedSong)}&part=snippet&key=${apiKey}`;
+
+  // Realizar la solicitud HTTP a la API de YouTube
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+      const startMinutes = 1; // Replace with the desired start minute
+      const startSeconds = 5; // Replace with the desired start second
+      if (data.items && data.items.length > 0) {
+        const videoId = data.items[0].id.videoId;
+        if (videoId) {
+          
+          const videoUrl = `https://www.youtube.com/watch?v=${videoId}&t=${startMinutes}m${startSeconds}s`;
+          
+           
+        window.open(videoUrl);
+        } else {
+          alert('No se encontraron resultados.');
+        }
+      } else {
+        alert('No se encontraron resultados.');
+      }
+    })
+    .catch(error => {
+      console.error('Error al realizar la búsqueda:', error);
+    });
+
+  //------
+  
+ /* window.open(`https://www.youtube.com/results?search_query=${encodeURIComponent(selectedSong)}`);*/
   songList.splice(randomIndex, 1)
 }else{
     emptyList.style.display = "block";
@@ -55,4 +90,9 @@ function mostrarCancion(){
 }}
 
 
-
+/* var iframe = document.createElement('iframe');
+            iframe.src = youtubeEmbedUrl;
+            iframe.width = '560'; // Set the desired width
+            iframe.height = '315'; // Set the desired height
+            iframe.allowFullscreen = false; // Enable fullscreen
+            document.getElementById('youtube-video').appendChild(iframe); */
