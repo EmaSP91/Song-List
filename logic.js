6e20143b41d3bd227abd86d270f2c1e172150bbb
Ema Aliaga <emakpop91@gmail.com>
2023-08-12 03:12:17 -0300

    Rename variables, classes and IDs to match the movies/series implementation

--- logic.js
+++ logic.js
@@ -1,12 +1,12 @@
-const songForm = document.getElementById("song-form");
-const song = document.getElementById("song");
-const cancionAzar = document.querySelector(".cancion-azar");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const random = document.querySelector(".random");
 const newGame = document.querySelector(".new-game");
 
 
 const emptyList = document.querySelector(".empty-list");
 
-let songList=[];
+let itemsList=[];
 
 
 let howto=document.querySelector(".howto");
@@ -28,37 +28,37 @@ function hideInstructions(){
 }
 
 
-songForm.addEventListener('submit', function(event){
+form.addEventListener('submit', function(event){
     event.preventDefault();
-    let chosenSong =  song.value
-    if(song.value != ""){
-    songList.push(chosenSong)
-    localStorage.setItem("Choices", songList.join(", "));
+    let chosenItem =  input.value
+    if(input.value != ""){
+    itemsList.push(chosenItem)
+    localStorage.setItem("Choices", itemsList.join(", "));
     }
-    console.log(song.value)
-    song.value= ""
-    console.log(song.value)
+    console.log(input.value)
+    input.value= ""
+    console.log(input.value)
        
 })
-cancionAzar.addEventListener('click', mostrarCancion);
+random.addEventListener('click', showItem);
 
 
 
-function mostrarCancion(){
-    if (songList.length != 0){
+function showItem(){
+    if (itemsList.length != 0){
 
   
   emptyList.style.display = "none";
-  const randomIndex =  Math.floor(Math.random() * songList.length);
+  const randomIndex =  Math.floor(Math.random() * itemsList.length);
   console.log(randomIndex)
-  const selectedSong = songList[randomIndex];
-  console.log(songList)
-  console.log(selectedSong)
+  const selectedItem = itemsList[randomIndex];
+  console.log(itemsList)
+  console.log(selectedItem)
   //------
  
   const apiKey = 'AIzaSyAVaBHWD19qKYWHRrPm1L4wkOHT6bwZ2UQ';
   
-  const url = `https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(selectedSong)}&part=snippet&key=${apiKey}`;
+  const url = `https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(selectedItem)}&part=snippet&key=${apiKey}`;
 
   // Realiza la solicitud HTTP a la API de YouTube
   fetch(url)
@@ -88,15 +88,15 @@ function mostrarCancion(){
   //------
   
  
-  songList.splice(randomIndex, 1)
+  itemsList.splice(randomIndex, 1)
 }else{
     emptyList.style.display = "block";
-    console.log("No hay canciones en la lista")
+    console.log("No hay nada en la lista")
 }}
 
 newGame.addEventListener('click', function(){
   localStorage.clear();
-  songList = [];
+  itemsList = [];
 });
 
 /*--------*/
