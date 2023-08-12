d61ed779113690fc67c2680e19ddaf8e98572713
Ema Aliaga <emakpop91@gmail.com>
2023-08-12 04:05:27 -0300

    add images for the movies/series background

--- toggle.js
+++ toggle.js
@@ -1,3 +1,5 @@
+
+
 const toggleContainer= document.querySelector(".toggle-container");
 
 const toggleSwitch = document.querySelector(".toggle-switch");
@@ -6,6 +8,13 @@ const body = document.querySelector("body");
 const title = document.querySelector(".title");
 const music = document.querySelector(".music");
 const question = document.querySelector(".question");
+const film = document.querySelectorAll(".film");
+const movie = document.querySelectorAll(".movie");
+const green = document.querySelectorAll(".green");
+const pink = document.querySelectorAll(".pink");
+
+
+
 
 let darkMode = localStorage.getItem("darkmode") === "true";
 
@@ -30,6 +39,15 @@ title.textContent = "Agrega una película/serie";
 
 music.src = "img/undraw_video.png"
 question.src = "img/undraw_Faq_dark.png"
+random.textContent ="P/S Aleatoria"
+film.forEach(element => {
+  element.src ="img/film.png"
+  
+});
+movie.forEach(element => {
+  element.src ="img/movie.png"
+  
+});
 //console.log("Dark: "+darkMode)
 
     }else{
@@ -38,7 +56,15 @@ body.classList.remove("dark");
 toggleSwitch.classList.remove("active");  
 music.src = "img/undraw_happy_music.png"
 question.src = "img/undraw_Faq.png" 
-title.textContent = "Agrega una canción"     
+title.textContent = "Agrega una canción"   
+green.forEach(element => {
+  element.src ="img/green.png"
+  
+});  
+pink.forEach(element => {
+  element.src ="img/pink.png"
+  
+});
  //console.log("Dark: "+darkMode)
 
     
